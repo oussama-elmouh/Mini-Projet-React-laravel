aed27e51655532213606081142adc881bb49266f
EL MOUH Oussama <ouss123maelmouh@gmail.com>
2025-10-15 20:36:44 +0200

    controlled component SingUp P

--- front_end/src/SignUp.js
+++ front_end/src/SignUp.js
@@ -9,11 +9,13 @@ export default function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordR, setPasswordR] = useState("");
+  const [accept, setAccept] = useState(false);
 
   // Fonction appelée quand l’utilisateur clique sur "Submit"
   function Submit(e) {
     // Empêche le rechargement de la page (comportement par défaut des formulaires HTML)
     e.preventDefault();
+    setAccept(true)
 
     // Pour le moment, on peut simplement vérifier que les valeurs sont bien capturées :
     // console.log(name);
@@ -40,7 +42,7 @@ export default function SignUp() {
           value={name} // valeur liée à l’état "name"
           onChange={(e) => setName(e.target.value)} // met à jour "name" à chaque saisie
         />
-
+        {name === '' && accept && <p className="error">username is required</p>}
         {/* Champ de l’email */}
         <label htmlFor="email" className="form-label">
           Email:
@@ -67,7 +69,10 @@ export default function SignUp() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-
+        {/* controle de password */}
+        {password.length < 8 && accept && (
+            <p className="error">Password must be mor then 8 character</p>
+        )}
         {/* Confirmation du mot de passe */}
         <label htmlFor="resetPassword" className="form-label">
           Reset Password:
@@ -80,6 +85,8 @@ export default function SignUp() {
           value={passwordR}
           onChange={(e) => setPasswordR(e.target.value)}
         />
+        {/* Controle rest password */}
+        {passwordR !== password && accept && <p className="error">password does not match</p>}
 
         {/* Boutons d’action */}
         <div className="form-actions">
